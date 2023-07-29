57471333fcf55801af0daeeabc27dcd2b22fc72f
AMEESHA SHAIK <140043042+ameeshadev@users.noreply.github.com>
2023-07-29 06:27:51 +0530

    Create user input.c

---
+++ user input.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    //creat a variable number that will store a number we get a output
+    int mynum;
+    // ask the user type a number
+    printf("enter a number press the enter");
+    //get and save the number you get the user
+    scanf("%d", &mynum);
+    //printf(the number is the user input)
+    printf("your number is: %d", mynum);
+    return 0;
+    
+    
+}
